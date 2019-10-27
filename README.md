0c9263d28bb37434816b11f782bfe7cd92c39920
noklam <channoklam@yahoo.com.hk>
2019-10-27 18:11:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# NeuRIPS2019 App
+This is a simple app for exploring the papers of NeuRIPS 2019. As I found that there are way too many papers and the official site did not provide any categorization, so I build this app for better exploration.
+
+# Tools
+* Heroku (For deployment)
+* streamlit (the app)
+* pandas, request and beautifulsoup4 for parsing the information
+
+
+Resource:
+list of keywords borrowed from:
+https://github.com/dcharrezt/NeurIPS-2019-Stats/blob/master/data/handcounted_inst.csv
+
+# Setup
+# install heroku cli for deployment (local)
+sudo snap install --classic heroku
---
+++ parser.py
@@ -0,0 +1,100 @@
+# %%
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import streamlit as st
+
+# %% [markdown]
+# A simple script to grabs all authors and papers name
+
+# %%
+url = 'https://nips.cc/Conferences/2019/AcceptedPapersInitial'
+res = requests.get(url)
+soup = BeautifulSoup(res.content, "lxml")
+
+titles= list(map(lambda x:x.text, soup.select("div > p > b")))
+authors=list(map(lambda x:x.text, soup.select("div > p > i")))
+
+
+# %%
+papers_df = pd.DataFrame(list(zip(titles, authors)),columns=['Title', 'Author'])
+# Output to a csv
+# papers_df.to_csv('papers.csv', index=False)
+
+# %%
+# class Paper:
+#     def __init__(self, title, author):
+#         self.title=title
+#         self.author=author
+    
+#     def __repr__(self):
+#         return f"Title: {self.title} \nAuthor(s): {self.author}\n"
+
+# %%
+class Filter:
+    def __init__(self, papers):
+        self.papers = papers_df
+    
+    def __call__(self, filter_str):
+        print('call')
+        result = self.filter_by_text(filter_str)
+        return result
+    
+    def __getattr__(self, name):
+        print('overload attr')
+        return getattr(self.papers,name)
+        
+    def filter_by_text(self, filter_str):
+        filter_str = filter_str.lower()
+        idx = self.Author.str.lower().str.contains(filter_str)
+        return self.papers[idx]
+    
+    def filter_by_keyword(self, filter_str):
+        result = []
+        filters = filter_str.split(",")
+        for paper in self.papers:
+            for author in paper.author:
+                for filter in filters:
+                    if filter in author:
+                        result.append(paper)
+                        continue
+
+        return result
+
+
+
+# %%
+# papers = [Paper(t,a) for t,a in zip(titles, authors)]
+
+# %%
+filter =Filter(papers_df)
+
+# %% [markdown]
+# # keyword
+
+# %%
+keywords_df = pd.read_csv('handcounted_inst.csv')
+
+# %%
+keywords = keywords_df.keywords
+
+# %%
+filter.filter_by_text(keywords[1])
+
+# %%
+keywords_df.keywords
+
+# %%
+st.title('Hello~')
+
+# %%
+filter_input = st.text_input('Filter by author')
+st.write(filter.filter_by_text(filter_input))
+
+# %%
+st.write('Hello')
+
+# %%
+
+# %%
